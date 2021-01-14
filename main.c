824ca266af4cc496b2ac83cf25da42eb0afe8cfd
David Sheffield <sheffield.david@gmail.com>
2021-01-14 08:15:15 -0800

    get cs and ss functions

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
+
 void iret_func(void);
 
+uint64_t get_cs(void);
+uint64_t get_ss(void);
+
 int main(int argc, char *argv[]) {
+  printf("cs = %zu, ss = %zu\n", get_cs(), get_ss());
   iret_func();
   printf("after iret!\n");
   return 0;
